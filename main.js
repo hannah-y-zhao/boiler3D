87d9acb105a6e05b083fe61108739619c51589ad
hannahyuwen <hz2788@nyu.edu>
2024-03-26 20:25:40 -0400

    first

---
+++ main.js
@@ -0,0 +1,62 @@
+//run: npm run dev OR sudo npm run dev
+
+import "./style.css";
+import * as THREE from "three";
+import { addBoilerPlateMesh,addStandardMesh } from "./addMeshes";
+import { addLight } from "./addLights";
+
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  100
+); //fov, aspect ratio, near, far
+camera.position.set(0,0,5)
+const meshes={}
+
+init();
+
+function init() {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  // const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // const material = new THREE.MeshBasicMaterial({ color: 0xff0000 }); //red
+  // const mesh = new THREE.Mesh(geometry, material);
+  // mesh.position.set(0, 0, -5);
+  // scene.add(mesh);
+  // meshes.default=mesh
+  // scene.add(meshes.default)
+
+  //meshes
+  meshes.default=addBoilerPlateMesh()
+  meshes.standard=addStandardMesh()
+
+  //lights
+  meshes.defaultLight=addLight()
+
+  //scene operationss
+  scene.add(meshes.default)
+  scene.add(meshes.standard)
+  scene.add(meshes.defaultLight)
+
+  resize()
+  animate();
+}
+function resize(){
+  window.addEventListener("resize",()=>{
+    renderer.setSize(window.innerWidth,window.innerHeight)
+    camera.aspect=window.innerWidth/window.innerHeight
+    camera.updateProjectionMatrix()
+  })
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  // meshes.default.position.x+=0.01
+  meshes.default.rotation.z+=0.01
+  meshes.standard.rotation.z-=0.01
+  meshes.standard.rotation.x+=0.01
+  renderer.render(scene, camera);
+}
